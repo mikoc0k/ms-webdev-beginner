5ab34d31b0c9b09bae5df354a02e37c6cc72bd54
mikoc0k <uciharis@hotmail.com>
2025-09-18 23:49:44 +0700

    belajar shallow copy

--- basic-js-array-loop.js
+++ basic-js-array-loop.js
@@ -13,3 +13,17 @@ while (i < eskrimVarian.length){
     console.log(`${i}. ${eskrimVarian[i]}`);
     i++;
 }
+
+// copy operation array di javascript adalah shallow copy (copy dan orinya share properties yang sama / punya reference yg sama)
+// tidak berlaku jika array berisi elemen primitif
+// terlihat jika diisi objek
+let jembod = [{otak: 1},{kelamin: 3},{umur: 77}]
+let copyJembod = jembod;
+console.log(jembod)
+console.log(copyJembod);
+jembod[1] = 123; // ori berubah
+console.log(jembod)
+console.log(copyJembod); // kwe berubah
+let copySuperJembod = Array.from(jembod); // swallow copy
+console.log(copySuperJembod); 
+
