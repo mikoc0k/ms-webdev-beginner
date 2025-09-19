382d3449d84fea5254f894130c4f17186b682e9b
mikoc0k <uciharis@hotmail.com>
2025-09-19 14:20:39 +0700

    method yang memiliki spesial treatmen kpd slot kosong di array

--- basic-js-array-loop.js
+++ basic-js-array-loop.js
@@ -17,13 +17,30 @@ while (i < eskrimVarian.length){
 // copy operation array di javascript adalah shallow copy (copy dan orinya share properties yang sama / punya reference yg sama)
 // tidak berlaku jika array berisi elemen primitif
 // terlihat jika diisi objek
-let jembod = [{otak: 1},{kelamin: 3},{umur: 77}]
+let jembod = [{otak: 1},{kelamin: 3},{umur: 77},];
 let copyJembod = jembod;
 console.log(jembod)
 console.log(copyJembod);
 jembod[1] = 123; // ori berubah
 console.log(jembod)
-console.log(copyJembod); // kwe berubah
+console.log(copyJembod); // KW berubah
 let copySuperJembod = Array.from(jembod); // swallow copy
-console.log(copySuperJembod); 
+console.log(copySuperJembod);
 
+let buah = [];
+// push method
+buah.push('mangga', 'pisang', 'jambu', 'stroberi');
+console.log(buah);
+buah[5]=666; // buah hanya punya 4 element dg indeks 0 s.d 3
+// dipush array ke-5. maka array -4 ditambahkan jg dengan kondisi kosong
+console.log(buah);
+console.log(buah[4]) // undefined
+
+// beberapa method array memiliki spesial treatmen kpd slot kosong/ masih menghandle slot kosong
+// contoh yang tidak spesial treatment
+buah.forEach((item,index)=>{
+    console.log(`${index}. ${item}`);
+}) // index-4 kosong tidak dicetak
+
+buah.reverse(buah);
+console.log(buah); // elemen yang kosong ditampilkan (special treatment)
